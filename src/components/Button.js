2f42658b812d5e3c51cd83e7d73ab516a52a1437
Theodor17 <45297151+Theodor17@users.noreply.github.com>
2021-08-02 12:24:35 +0100

    Add files via upload

---
+++ src/components/Button.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import './Button.css';
+
+class Button extends Component{
+
+    render(){
+
+        return(
+
+            <div className = "Button"
+            
+                onClick = {this.props.onClick}
+                data-value = {this.props.value}
+                
+                >
+
+                {this.props.label}
+
+            </div>
+
+        )
+
+    }
+
+}
+
+export default Button;
